90c5eda2c207b1342f324f22555eb33d5bae105c
mazamat <azamat.murat2002@mail.ru>
2024-01-25 11:32:15 +0600

    add price filter

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -7,6 +7,7 @@ class Car
     public string? Brand { get; set; }
     public string? Model { get; set; }
     public int Year { get; set; }
+    public string? Price { get; set; }
 }
 
 class Program
@@ -15,14 +16,18 @@ class Program
     {
         List<Car> cars = new List<Car>
         {
-            new Car { Brand = "Lexus", Model = "ES 250", Year = 2020 },
-            new Car { Brand = "Lexus", Model = "LX 570", Year = 2019 },
-            new Car { Brand = "Kia", Model = "K5", Year = 2021 },
-            new Car { Brand = "Honda", Model = "Accord", Year = 2018 },
-            new Car { Brand = "Hyundai", Model = "Elantra", Year = 2020 }
+            new Car { Brand = "Lexus", Model = "ES 250", Year = 2020,Price = "$53,340"},
+            new Car { Brand = "Lexus", Model = "LX 570", Year = 2019, Price = "$89,999"},
+            new Car { Brand = "Kia", Model = "K5", Year = 2021,Price = "$25,390"},
+            new Car { Brand = "Honda", Model = "Accord", Year = 2021, Price = "$24,970"},
+            new Car { Brand = "Hyundai", Model = "Elantra", Year = 2020 , Price = "$20,655"}
         };
+        var priceFilter = cars.OrderByDescending(car => car.Price);
+        Console.WriteLine("\nСписок машин, отсортированных по цене: ");
+        PrintPrice(priceFilter);
+        
         var filteredCars = cars.Where(car => car.Brand == "Lexus");
-        Console.WriteLine("Модели машины \"Lexus\":");
+        Console.WriteLine("\nМодели машины \"Lexus\":");
         PrintCars(filteredCars);
 
         var sortedCars = cars.OrderByDescending(car => car.Year);
@@ -44,6 +49,14 @@ class Program
         foreach (var car in cars)
         {
             Console.WriteLine($"{car.Model} ({car.Year})");
+        }
+    }
+
+    static void PrintPrice(IEnumerable<Car> cars)
+    {
+        foreach (var car  in cars)
+        {
+            Console.WriteLine($"{car.Brand} {car.Model} - {car.Price}");
         }
     }
 }
--- LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e42c772bce8ad331611e0d08bf2aa50dc66651")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe489b4ed932db294ab5aabe1488fa163a1c6a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
