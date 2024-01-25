4fe489b4ed932db294ab5aabe1488fa163a1c6a8
mazamat <azamat.murat2002@mail.ru>
2024-01-25 11:21:01 +0600

    sort cars

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Car
+{
+    public string? Brand { get; set; }
+    public string? Model { get; set; }
+    public int Year { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Car> cars = new List<Car>
+        {
+            new Car { Brand = "Lexus", Model = "ES 250", Year = 2020 },
+            new Car { Brand = "Lexus", Model = "LX 570", Year = 2019 },
+            new Car { Brand = "Kia", Model = "K5", Year = 2021 },
+            new Car { Brand = "Honda", Model = "Accord", Year = 2018 },
+            new Car { Brand = "Hyundai", Model = "Elantra", Year = 2020 }
+        };
+        var filteredCars = cars.Where(car => car.Brand == "Lexus");
+        Console.WriteLine("Модели машины \"Lexus\":");
+        PrintCars(filteredCars);
+
+        var sortedCars = cars.OrderByDescending(car => car.Year);
+        Console.WriteLine("\nМодели машин, отсортированные по году выпуска (в порядке убывания):");
+        PrintCars(sortedCars);
+
+        var groupedCars = cars.GroupBy(car => car.Brand);
+        Console.WriteLine("\nМодели машин по маркам:");
+        foreach (var group in groupedCars)
+        {
+            Console.WriteLine($"Марка: {group.Key}");
+            PrintCars(group);
+            Console.WriteLine();
+        }
+    }
+
+    static void PrintCars(IEnumerable<Car> cars)
+    {
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} ({car.Year})");
+        }
+    }
+}
